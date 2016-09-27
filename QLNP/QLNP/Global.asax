cb9200c495564468f898f30783c79c140e63d7f8
DoAnTDA <phamducanhk36@gmail.com>
2016-09-27 16:04:17 +0700

    Create Project

---
+++ QLNP/QLNP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QLNP.MvcApplication" Language="C#" %>
---
+++ QLNP/QLNP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QLNP.Startup))]
+namespace QLNP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
